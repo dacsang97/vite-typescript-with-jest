fda3f24388ab54f6ab6c57c2309152493be9f332
EvanN <dacsang97@gmail.com>
2020-09-11 20:24:16 +0700

    feat: init template

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,24 @@
+<template>
+  <h1>{{ msg }}</h1>
+  <button @click="count++">count is: {{ count }}</button>
+  <p>
+    Edit <code>components/HelloWorld.vue</code> to test hot module replacement.
+  </p>
+</template>
+
+<script lang="ts">
+import { ref } from "vue";
+
+export default {
+  name: "HelloWorld",
+  props: {
+    msg: String,
+  },
+  setup() {
+    const count = ref(0);
+    return {
+      count,
+    };
+  },
+};
+</script>
---
+++ src/components/specs/HelloWorld.spec.ts
@@ -0,0 +1,26 @@
+import { mount } from "@vue/test-utils";
+import HelloWorld from "../HelloWorld.vue";
+
+describe("HelloWorld.vue", () => {
+  const msg = "Hello World";
+  it("renders correctly", () => {
+    const wrapper = mount(HelloWorld, {
+      props: {
+        msg,
+      },
+    });
+    expect(wrapper.text()).toContain(msg);
+    expect(wrapper.html()).toMatchSnapshot();
+  });
+
+  it("counter increment exactly", async () => {
+    const wrapper = mount(HelloWorld, {
+      props: {
+        msg,
+      },
+    });
+    expect(wrapper.find("button").text()).toContain("0");
+    await wrapper.find("button").trigger("click");
+    expect(wrapper.find("button").text()).toContain("1");
+  });
+});
